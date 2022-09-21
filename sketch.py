93db3bc910113c9671c777d82d19fcf26d6ca096
Sanjeev Pathak <pathaksanjeev0496@gmail.com>
2022-09-21 11:35:58 +0530

    made sketches of famous peoples

---
+++ sketch.py
@@ -0,0 +1,7 @@
+# Fire This command in your command prompt
+# pip install sketchpy
+# once it downloaded than fire below command and see the magic !
+
+from sketchpy import library as imp
+obj = imp.apj()
+obj.draw()
